dfa84fa4cd8263531a7b53bf3e1b654c85ede42e
vinilsmelo <99348430+vinilsmelo@users.noreply.github.com>
2024-02-04 20:08:04 -0300

    Delete waste_management directory

--- waste_management/waste_management/urls.py
+++
@@ -1,9 +0,0 @@
-# waste_management/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('waste_app.urls')),
-]
